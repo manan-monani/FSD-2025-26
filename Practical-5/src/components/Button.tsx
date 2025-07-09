04146cc855998a9308f5091f9dcecbdff4708154
Manan Monani <155767948+manan-monani@users.noreply.github.com>
2025-07-09 16:07:33 +0000

    practical-5 complete

--- Practical-5/src/components/Button.tsx
+++ Practical-5/src/components/Button.tsx
@@ -1,16 +1,14 @@
+// src/components/Button.tsx
+
 import React from 'react';
 
 interface ButtonProps {
-    label: string;
-    onClick: () => void;
+  label: string;
+  onClick: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-    return (
-        <button onClick={onClick}>
-            {label}
-        </button>
-    );
+  return <button onClick={onClick}>{label}</button>;
 };
 
 export default Button;
